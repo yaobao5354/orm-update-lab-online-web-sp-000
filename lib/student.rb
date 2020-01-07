7f89bbe7fcc539bb8e989befb1e5b61646a48970
yaoyu bao <yaobao5354@gmail.com>
2020-01-07 04:23:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,6 +3,12 @@ require_relative "../config/environment.rb"
 class Student
   attr_accessor :id, :name, :grade
 
+  def initialize(id = nil, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
+  end 
+
   def self.new_from_db(row)
     new_student = self.new
     new_student.id = row[0]
