e8fcaa1de1929f3273e5873221288402c392e6a1
yaoyu bao <yaobao5354@gmail.com>
2020-01-07 04:38:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -45,7 +45,7 @@ class Student
   end
 
   def update
-    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    sql = "UPDATE students SET name = ?, grade = ?, WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
