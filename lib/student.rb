ee6a09d6e7a6815fb0b3682240019781bee7002b
yaoyu bao <yaobao5354@gmail.com>
2020-01-07 04:48:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,10 +12,7 @@ class Student
 
   def self.new_from_db(row)
     binding.pry
-    new_student = self.new
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
+    new_student = self.new(row[0],row[1],row[2])
     new_student
   end
 
