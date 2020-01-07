72e79aa548e7575a5784b42707409f9e534a8b60
yaoyu bao <yaobao5354@gmail.com>
2020-01-07 04:20:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,15 @@
 require_relative "../config/environment.rb"
 
 class Student
+  attr_accessor :id, :name, :grade
 
+  def self.new_from_db(row)
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
